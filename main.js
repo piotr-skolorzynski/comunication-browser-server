409ed406e129fe6b21c71086c996aae3d92af7f4
Piotr <p.skolorzynski@gmail.com>
2021-07-13 13:38:23 +0200

    modify main.js render users using async/await

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => {
-        const list = document.querySelector('[data-id="list"]');
-        let html = '';
-        users.forEach(user => {
-            html += `<li class="user" data-id="${user.id}">
-                        ${user.name} lives on ${user.address.street} street in ${user.address.city} and works in ${user.company.name}            
-                    </li>`
-        });
-        list.innerHTML = html;
-    })
+
+// const renderUsers = () => {
+//     const url = 'https://jsonplaceholder.typicode.com/users';
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(users => {
+//             const list = document.querySelector('[data-id="list"]');
+//             let html = '';
+//             users.forEach(user => {
+//                 html += `<li class="user" data-id="${user.id}">
+//                             ${user.name} lives on ${user.address.street} street in ${user.address.city} and works in ${user.company.name}            
+//                         </li>`
+//             });
+//             list.innerHTML = html;
+//         })
+// };
+
+const createUsersList = users => {
+    const list = document.querySelector('[data-id="list"]');
+    let html = '';
+    users.forEach(user => {
+        html += `<li class="user" data-id="${user.id}">
+                    ${user.name} lives on ${user.address.street} street in ${user.address.city} and works in ${user.company.name}            
+                </li>`;
+    });
+    list.innerHTML = html;
+}
+
+async function renderUsers() {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    const response = await fetch(url);
+    const users = await response.json();
+    createUsersList(users);
+}
+
+
+document.addEventListener('DOMContentLoaded', renderUsers);
